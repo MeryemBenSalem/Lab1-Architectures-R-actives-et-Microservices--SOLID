fe6a057312e51ecc74840793c23e6e89e2837c3a
Cherifa3 <123500362+Cherifa3@users.noreply.github.com>
2025-10-29 23:14:51 +0100

    Create CustomFileReader.java

---
+++ DIP/src/com/directi/training/dip/exercice_refactored/CustomFileReader.java
@@ -0,0 +1,30 @@
+package com.directi.training.dip.exercise_refactored;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CustomFileReader implements Reader
+{
+    private String _fileName;
+
+    public CustomFileReader(String fileName)
+    {
+        _fileName = fileName;
+    }
+
+    @Override
+    public String read()
+    {
+        StringBuilder content = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(_fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+}
