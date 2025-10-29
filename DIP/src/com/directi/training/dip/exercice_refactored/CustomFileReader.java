09a0b2db6306243251974c309f497996af680ecd
Cherifa3 <123500362+Cherifa3@users.noreply.github.com>
2025-10-29 23:15:35 +0100

    Update CustomFileReader.java

--- DIP/src/com/directi/training/dip/exercice_refactored/CustomFileReader.java
+++ DIP/src/com/directi/training/dip/exercice_refactored/CustomFileReader.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class CustomFileReader implements Reader
+public class CustomFileReader implements IReader
 {
     private String _fileName;
 
